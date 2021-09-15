c4452d7de73f5e1fcf85f7e7076c85e4058b745e
Cae Lundin <tecc@tecc.me>
2021-09-15 07:06:22 +0000

    Create Web IDE configuration, fix src/util.ts
    src/util.ts had an import to `path/posix`, however the intention is to
    import `path`.

--- src/util.ts
+++ src/util.ts
@@ -1,15 +1,11 @@
-
-
 /*
  * SPDX-FileCopyrightText: 2021 Cae Lundin <tecc@tecc.me>
  *
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-KDE-Accepted-LGPL
  */
 
-//
 
-
-import path from "path/posix";
+import path from "path";
 
 export type DefaultGlobalKey = (keyof (typeof global));
 export type ExtraGlobalKey = 'packageDir';
