a8d5f91db85769758733d3d8a7a36b6d4854dad6
Cae Lundin <tecc@tecc.me>
2021-09-15 14:10:14 +0200

    Remove hashbang from src/render.ts
    Hashbangs are not valid for TypeScript files, and as such were causing compile errors.
    Even then, src/render.ts is not intended for execution.

--- src/render.ts
+++ src/render.ts
@@ -1,15 +1,8 @@
-
-
 /*
  * SPDX-FileCopyrightText: 2021 Cae Lundin <tecc@tecc.me>
  *
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-KDE-Accepted-LGPL
  */
-
-//
-
-
-#!/usr/bin/env node
 
 import fs from 'fs';
 import fm from 'front-matter';
