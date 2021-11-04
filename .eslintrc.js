936f84f70ff8f3693e498bb6d71b26bf11da88df
tecc <tecc@tecc.me>
2021-11-04 21:21:45 +0100

    Modify linter rules to allow for fallthrough

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,6 +34,7 @@ module.exports = {
             "always"
         ],
         "@typescript-eslint/no-non-null-assertion": "off",
-        "@typescript-eslint/explicit-module-boundary-types": "off"
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "no-fallthrough": "off"
     }
 };
