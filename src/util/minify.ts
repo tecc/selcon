f7e451d09f22f4d574f4386dcc98eff4a09688d3
tecc <tecc@tecc.me>
2021-11-04 20:42:25 +0100

    Improve minifying system, add CSS minifying

--- src/util/minify.ts
+++ src/util/minify.ts
@@ -1,6 +1,23 @@
 import terser from "terser";
 import { escapeStringRegexp } from "@/util";
 import HTMLMinifier from "html-minifier-terser";
+import Log from "@/log";
+import csso from "csso";
+
+function minified(type: string, input: string, output?: string): string {
+    if (!output) {
+        Log.warn("Couldn't minify, returning original");
+        return input;
+    }
+    const sizeFactor = (output.length / input.length) * 100;
+    if (output.length > input.length) {
+        Log.warn(`Minify output length (${output.length}) greater than input length (${input.length}) (${sizeFactor}% of original size), returning input`)
+        return input;
+    } else {
+        Log.debug(`Minified ${type} from`, input.length, "to", output.length, "(" + sizeFactor + "% of original size)");
+        return output;
+    }
+}
 
 export interface JSMinifyOptions {
     obfuscate: boolean;
@@ -12,7 +29,7 @@ export const DEFAULT_JS_OPTIONS: JSMinifyOptions = {
     preservedNames: [],
     inlining: true
 };
-export async function js(input: string, optionsPart: Partial<JSMinifyOptions>): Promise<string> {
+export async function js(input: string, optionsPart: Partial<JSMinifyOptions> = {}): Promise<string> {
     const options = Object.assign({}, DEFAULT_JS_OPTIONS, optionsPart);
     const result = await terser.minify(input, {
         mangle: {
@@ -24,17 +41,46 @@ export async function js(input: string, optionsPart: Partial<JSMinifyOptions>): 
             inline: options.inlining
         }
     });
-    return result.code!;
+    return minified("JS", input, result.code);
 }
 
 // TODO: Options
-export async function html(code: string): Promise<string> {
-    return await HTMLMinifier.minify(code, {
+export interface HTMLMinifyOptions {
+
+}
+export const DEFAULT_HTML_OPTIONS: HTMLMinifyOptions = {};
+export async function html(code: string, optionsPart: Partial<HTMLMinifyOptions> = {}): Promise<string> {
+    const options: HTMLMinifyOptions = Object.assign({}, DEFAULT_HTML_OPTIONS, optionsPart);
+    const result = await HTMLMinifier.minify(code, {
         minifyCSS: false,
         minifyJS: false,
         minifyURLs: false,
         html5: true,
         preserveLineBreaks: false,
-        removeComments: true
+        removeComments: true,
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true,
+        caseSensitive: true,
+        conservativeCollapse: true,
+        removeOptionalTags: true,
+        keepClosingSlash: true
     });
+    return minified("HTML", code, result);
+}
+
+export interface CSSMinifyOptions {
+    useUnsafeOptions: boolean
+}
+export const DEFAULT_CSS_OPTIONS: CSSMinifyOptions = {
+    useUnsafeOptions: true
+};
+export function css(code: string, optionsPart: Partial<CSSMinifyOptions> = {}) {
+    const options: CSSMinifyOptions = Object.assign({}, DEFAULT_CSS_OPTIONS, optionsPart);
+    const result = csso.minify(code, {
+        sourceMap: false,
+        comments: true,
+        forceMediaMerge: options.useUnsafeOptions,
+        restructure: true
+    });
+    return minified("CSS", code, result.css);
 }
